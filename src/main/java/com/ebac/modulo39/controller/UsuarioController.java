a4c1167f147dca216eacad6ca05c05d6fe5d5012
SPCruzaley <sp_cruzaley@hotmail.com>
2023-08-02 17:31:25 -0600

    Modulo 40

--- src/main/java/com/ebac/modulo39/controller/UsuarioController.java
+++ src/main/java/com/ebac/modulo39/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.ebac.modulo39.controller;
 
 import com.ebac.modulo39.dto.Usuario;
+import com.ebac.modulo39.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,48 +12,63 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    UsuarioService usuarioService;
+
     @GetMapping("/usuarios")
     public List<Usuario> obtenerUsuarios() {
         // Devuelve un objeto User que se convertirá automáticamente en JSON/XML en la respuesta.
-        return List.of(new Usuario());
+        return usuarioService.obtenerUsuarios();
     }
 
     @GetMapping("/usuarios/{id}")
-    public Usuario obtenerUsuarioPorId(@PathVariable Long id) {
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
         // Lógica para obtener el usuario por su ID
         // Devuelve un objeto User que se convertirá automáticamente en JSON/XML en la respuesta.
-        System.out.println("Id recibido: " + id);
-        return new Usuario();
+        Optional<Usuario> usuarioOptional = usuarioService.obtenerUsuarioPorId(id);
+
+        return usuarioOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/usuarios")
-    public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) throws URISyntaxException {
         // Lógica para crear un nuevo usuario
         // Retorna ResponseEntity con el objeto User en el cuerpo y un código de estado 201 (CREATED) en la respuesta.
-        System.out.println("Usuario recibido: " + usuario);
-        return ResponseEntity.ok(usuario);
+        usuarioService.crearUsuario(usuario);
+
+        return ResponseEntity.created(new URI("http://localhost/usuarios")).build();
     }
 
     @PutMapping("/usuarios/{id}")
     public ResponseEntity<Usuario> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
         // Lógica para actualizar el usuario con el ID proporcionado
         // Retorna ResponseEntity con el objeto User actualizado en el cuerpo y un código de estado 200 (OK) en la respuesta.
-        System.out.println("Id recibido: " + id);
-        System.out.println("Usuario recibido: " + usuarioActualizado);
-        return ResponseEntity.ok(usuarioActualizado);
+        Optional<Usuario> usuarioOptional = usuarioService.obtenerUsuarioPorId(id);
+
+        if (usuarioOptional.isPresent()) {
+            usuarioActualizado.setIdUsuario(usuarioOptional.get().getIdUsuario());
+            usuarioService.actualizarUsuario(usuarioActualizado);
+
+            return ResponseEntity.ok(usuarioActualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/usuarios/{id}")
     public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
         // Lógica para eliminar el usuario con el ID proporcionado
         // Retorna ResponseEntity con un código de estado 204 (NO_CONTENT) en la respuesta.
-        System.out.println("Id recibido: " + id);
-        // Si el usuario se eliminó exitosamente, respondemos con un código de estado 204 (NO_CONTENT) sin cuerpo de respuesta.
+        usuarioService.eliminarUsuario(id);
+
         return ResponseEntity.noContent().build();
     }
 }
